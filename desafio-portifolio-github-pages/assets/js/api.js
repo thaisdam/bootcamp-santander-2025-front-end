85f3e6c837cf2e43efce37addd70e781370fbb0b
Thais Damasceno Silva <70724923+thaisdam@users.noreply.github.com>
2025-08-31 16:31:50 -0300

    Add files via upload

---
+++ desafio-portifolio-github-pages/assets/js/api.js
@@ -0,0 +1,7 @@
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/bootcamp-santander-2025-front-end/desafio-portifolio-github-pages/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
