b34c44a6a891e1328a1058689cb692d65e0ca029
Thais Damasceno Silva <70724923+thaisdam@users.noreply.github.com>
2025-08-31 17:08:01 -0300

    Update api.js

--- desafio-portifolio-github-pages/assets/js/api.js
+++ desafio-portifolio-github-pages/assets/js/api.js
@@ -1,7 +1,6 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/thaisdam/bootcamp-santander-2025-front-end/desafio-portifolio-github-pages/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    const url = 'https://raw.githubusercontent.com/thaisdam/bootcamp-santander-2025-front-end/main/data/profile.json';
+    const response = await fetch(url);
+    const profileData = await response.json();
+    return profileData;
 }
